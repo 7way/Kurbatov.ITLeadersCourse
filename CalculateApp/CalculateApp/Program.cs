6c924fdde2a57ff64b5b6197a80b3e8abfe2bb96
Artem Kurbatov <mybox290@gmail.com>
2021-10-02 21:13:41 +0300

    Add CalculateApp

---
+++ CalculateApp/CalculateApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculateApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 1;
+            b = 2;
+            c = 3;
+
+            int sum = a + b + c;
+
+            Console.WriteLine(a + " + " + b + " + " + c + " = " + sum);
+        }
+    }
+}
